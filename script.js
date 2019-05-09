bcdde6af3dcaee03821f67ffdde0c3ae1c53d076
elevin14 <elevin14@gmail.com>
2019-05-08 22:33:10 -0700

    Create basic flex grid

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+function add_grid() {
+    const gridDiv = document.querySelector('.grid');
+
+    for (let j = 0; j < 16; j++) {
+
+        const col = document.createElement('div')
+
+        for (let i = 0; i < 16; i++) {
+
+            const box = document.createElement('div');
+            box.setAttribute('class', 'square');
+            col.appendChild(box);
+        }
+
+        gridDiv.appendChild(col)
+    }
+
+}
+
+add_grid()
 
 const buttons = document.querySelectorAll('.button')
 buttons.forEach((button) => {
